0899eaeb6047d85f7a374b86b793451177934425
Ganesh Chothve <84216414+GaneshChothve@users.noreply.github.com>
2021-08-25 21:32:04 +0530

    Update index.js

--- hamburger/index.js
+++ hamburger/index.js
@@ -6,4 +6,11 @@ const drag=document.getElementsByClassName('view2')[0]
 btn.addEventListener('click', () => {
     btn.classList.toggle('active')
     drag.classList.toggle('active')
+    k=btn.innerHTML
+    if(k=='☰'){
+        btn.innerHTML='X'
+    }
+    else{
+        btn.innerHTML='☰'
+    }
 })
