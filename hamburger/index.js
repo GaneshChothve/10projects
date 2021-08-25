138260b6a305bd045c1d3aa647853caeef94cced
Ganesh Chothve <84216414+GaneshChothve@users.noreply.github.com>
2021-08-25 21:17:27 +0530

    Update index.js

--- hamburger/index.js
+++ hamburger/index.js
@@ -2,17 +2,8 @@
 
 const btn= document.getElementsByClassName('view1')[0]
 const drag=document.getElementsByClassName('view2')[0]
-let flag=0
-btn.addEventListener('click', () => {
 
-    if(flag==0){
-        btn.style.marginLeft= `${76}%`
-        drag.style.marginLeft= `${80}%`
-        flag++
-    }
-    else{
-        btn.style.marginLeft= `${90}%`
-        drag.style.marginLeft= `${100}%`
-        flag=0
-    }
+btn.addEventListener('click', () => {
+    btn.classList.toggle('active')
+    drag.classList.toggle('active')
 })
