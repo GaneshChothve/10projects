5adf7c709519ee8b48d8e7092233ba08ef295c4a
Ganesh Chothve <84216414+GaneshChothve@users.noreply.github.com>
2021-08-25 20:39:19 +0530

    Update script.js

--- Carousal/script.js
+++ Carousal/script.js
@@ -5,7 +5,7 @@ let i=1
 setInterval(change,2000)
 function change(){
 
-    bg.style.transform= `translateX(${-400*i}px)`
+    bg.style.transform= `translateX(${-250*i}px)`
     i++
     if(i==3){i=0}
 }
